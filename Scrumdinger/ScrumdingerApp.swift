76f14bb2e6e3b5b4c70dbd841697f5c98fda0e03
MMTabaka <miloszmtabaka@gmail.com>
2022-06-05 01:01:33 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Milosz Tabaka on 05/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
