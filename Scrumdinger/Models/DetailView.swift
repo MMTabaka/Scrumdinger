701f37432d0e9325c134199f6cb26ca622d1ab1a
MMTabaka <miloszmtabaka@gmail.com>
2022-06-11 23:18:25 +0100

    Section 5: Navigate Between Screens

--- Scrumdinger/Models/DetailView.swift
+++ Scrumdinger/Models/DetailView.swift
@@ -13,9 +13,12 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                Label("Start Meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                    
+                }
                 HStack {
                     Label("Lenght", systemImage: "clock")
                     Spacer()
@@ -39,6 +42,7 @@ struct DetailView: View {
                 }
             }
         }
+        .navigationTitle(scrum.title)
     }
 }
 
